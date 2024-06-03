f0968c70bf22f3d4c9a7268417c4bcbfa2678ca5
Katya Raman <katyaraman@icloud.com>
2024-06-03 15:54:27 -0700

    set up search bar

--- LessWrong iOS/Views/ContentView.swift
+++ LessWrong iOS/Views/ContentView.swift
@@ -10,15 +10,49 @@ import CoreData
 import WebKit
 import UIKit
 
+
+
 struct ContentView: View {
+    var body : some View {
+        MainView()
+    }
+}
+
+struct MainView: View {
     @StateObject var networkManager = NetworkManager()
+    @ObservedObject var searchModel = SearchModel()
+    
+    var body : some View {
+        MainChildView()
+            .accentColor(.red)
+            .environmentObject(networkManager)
+            .environmentObject(searchModel)
+            .searchable(text: $searchModel.searchText, tokens: $searchModel.tokens) { token in
+                        switch token {
+                        case .newPosts:
+                            Text("New Posts")
+                        case .topPosts:
+                            Text("Top Posts")
+                        case .userPosts:
+                            Text("User")
+                        }
+                
+                
+            }
+    }
+}
+
+struct MainChildView: View {
+    @EnvironmentObject var networkManager: NetworkManager
     @Environment(\.colorScheme) var colorScheme
     @State private var selectedQueryType: QueryType = .topPosts
     @State private var postLimit: String = "10"
     @State private var username: String = "User"
-    @StateObject private var searchModel = SearchModel()
+    @EnvironmentObject var searchModel: SearchModel
     @Environment(\.customFont) var customFont: Font
-    
+    @Environment(\.isSearching) private var isSearching
+
+
     var body: some View {
         NavigationView {
             mainView()
@@ -27,7 +61,7 @@ struct ContentView: View {
                         LinearGradient(colors: [getTopBackgroundColor(),getBackgroundColor()], startPoint: .top, endPoint: .bottom)
                     }.ignoresSafeArea(.all)
                 }
-                
+                .scrollContentBackground(.hidden)
             .navigationTitle("Posts")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -110,30 +144,30 @@ struct ContentView: View {
     
     @ViewBuilder
     func mainView() -> some View {
-        ScrollView {
-            LazyVStack(spacing: 8) {
-                ForEach(networkManager.posts) { post in
-                    Section {
-                        postFrontView(post: post)
-                    }.background(getSectionColor().opacity(0.5))
-                }
-                .cornerRadius(10)
-                .padding(.horizontal)
-            }
-            .searchable(text: $searchModel.searchText, tokens: $searchModel.tokens) { token in
-                        switch token {
-                        case .newPosts:
-                            Text("New Posts")
-                        case .topPosts:
-                            Text("Top Posts")
-                        case .userPosts:
-                            Text("User")
+        if !isSearching {
+            ScrollView {
+                LazyVStack(spacing: 8) {
+                        ForEach(networkManager.posts) { post in
+                            Section {
+                                postFrontView(post: post)
+                            }.background(getSectionColor().opacity(0.5))
                         }
-                
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                }
+                .setNavigationBarTitleToLightMode()
                 
             }
-            .setNavigationBarTitleToLightMode()
-
+        } else {
+            List {
+                if searchModel.tokens.isEmpty && searchModel.searchText.isEmpty { //present possible tokens
+                    suggestedSearchView()
+                }
+                else {
+                    Text("Filtered posts based on query will go here")
+                }
+            }
+         
         }
     }
     
@@ -144,10 +178,9 @@ struct ContentView: View {
                 searchModel.tokens.append(.topPosts)
             } label: {
                 HStack {
-                    Image(systemName: "eye.fill")
+                    Image(systemName: "flame.fill")
                         .padding(.horizontal, 5)
-                    Text("Top Posts")
-                        .foregroundStyle(Color(UIColor.label))
+                    Text("Top Posts").foregroundStyle(getColor())
                 }
             }
             
@@ -155,11 +188,10 @@ struct ContentView: View {
                 searchModel.tokens.append(.newPosts)
             } label: {
                 HStack {
-                    Image(systemName: "paperclip")
+                    Image(systemName: "arrow.up")
                         .padding(.horizontal, 5)
                     
-                    Text("Entries with Media")
-                        .foregroundStyle(Color(UIColor.label))
+                    Text("New Posts").foregroundStyle(getColor())
                 }
             }
             
@@ -167,11 +199,10 @@ struct ContentView: View {
                 searchModel.tokens.append(.userPosts)
             } label: {
                 HStack {
-                    Image(systemName: "bell.fill")
+                    Image(systemName: "person.fill")
                         .padding(.horizontal, 5)
                     
-                    Text("Entries with Reminder")
-                        .foregroundStyle(Color(UIColor.label))
+                    Text("User").foregroundStyle(getColor())
                 }
             }
             
